f3a2bc2825567cd89ed19ece4c66b059624cdaf7
ChiriloviciDaniel <chirilovicidanielrafael@gmail.com>
2025-06-11 16:28:01 +0300

    gitignore created to dev

--- obj/Debug/net8.0/SkyProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/SkyProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f89c8794abacd9b24d7b12455eca16074e82927d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a824f7c01559d52a9780abcdc832d20483100286")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
